f80b98d2aa4530b3f5eef3bb367390769858b907
Noyon <mdtazrulislam14@gmail.com>
2022-10-18 22:01:42 +0600

    first day practice completed

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
 console.log("Rangpur Polytechnic Institute")
+
+var name = "Md Tazrul Islam";
+var age = "18";
+var address = "Rangpur";
+var college = "Rangpur Polytechnic Institute";
+var department = "Computer Science";
+var semester = "6th";
+var roll = "405888";
+var group = "B";
+console.log(name);
+console.log(age);
+console.log(address);
+console.log(college);
+console.log(department);
+console.log(semester);
+console.log(roll)
+console.log(group)
